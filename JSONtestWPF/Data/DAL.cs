32387b07b9531112673e302338d8f571c3d9467c
Anruina <anruiamv@hotmail.com>
2024-04-19 06:55:24 +0200

    addin crud

---
+++ JSONtestWPF/Data/DAL.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using JSONtestWPF.Models;
+
+namespace JSONtestWPF.Data
+{
+    public class DAL
+    {
+        private const string FilePath = "C:\\Users\\Rachel\\source\\repos\\Anruina\\JSONtestWPF\\JSONtestWPF\\data.json";
+
+        public List<Person> ReadDataFromFile()
+        {
+            List<Person> people = new List<Person>();
+
+            if (File.Exists(FilePath))
+            {
+                string jsonString = File.ReadAllText(FilePath);
+                people = JsonSerializer.Deserialize<List<Person>>(jsonString);
+            }
+
+            return people;
+        }
+
+        public void SaveDataToFile(List<Person> people)
+        {
+            string jsonString = JsonSerializer.Serialize(people);
+            File.WriteAllText(FilePath, jsonString);
+        }
+
+        public void CreateOrUpdatePerson(Person person)
+        {
+            List<Person> people = ReadDataFromFile();
+
+            var existingPerson = people.FirstOrDefault(p => p.Name == person.Name);
+            if (existingPerson != null)
+            {
+                existingPerson.Age = person.Age;
+            }
+            else
+            {
+                people.Add(person);
+            }
+
+            SaveDataToFile(people);
+
+        }
+
+        public void DeletePerson(string name)
+        {
+
+            List<Person> people = ReadDataFromFile();
+
+            var personToRemove = people.FirstOrDefault(p => p.Name == name);
+            if (personToRemove != null)
+            {
+                people.Remove(personToRemove);
+                SaveDataToFile(people);
+
+            }
+            else
+            {
+                Console.WriteLine($"Person with name '{name}' not found.");
+            }
+
+
+        }
+
+
+    }
+}
--- JSONtestWPF/MainWindow.xaml.cs
+++ JSONtestWPF/MainWindow.xaml.cs
@@ -1,24 +1,89 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using JSONtestWPF.Models;
+using System.Xml.Linq;
+using JSONtestWPF.Data;
 
 namespace JSONtestWPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly DAL dal;
+        private readonly List<Person> people;
+
         public MainWindow()
         {
             InitializeComponent();
+            dal = new DAL();
+            people = dal.ReadDataFromFile();
+            dataGrid.ItemsSource = people;
+        }
+
+        private void AddData_Click(object sender, RoutedEventArgs e)
+        {
+            // Read existing data from JSON file
+            List<Person> people = dal.ReadDataFromFile();
+
+            // Add new person
+            Person person = new Person
+            {
+                Name = txtName.Text,
+                Age = int.Parse(txtAge.Text)
+            };
+
+            // Save updated data to JSON file
+            dal.CreateOrUpdatePerson(person);
+
+            MessageBox.Show("Data added successfully.");
+        }
+
+/*        private void ModifyData_Click(object sender, RoutedEventArgs e)
+        {
+            Person selectedPerson = dataGrid.SelectedItem as Person;
+            if (selectedPerson != null)
+            {
+                CreateOrUpdate modifyWindow = new CreateOrUpdate(selectedPerson);
+                modifyWindow.ShowDialog();
+            
+                if (modifyWindow.PersonModified)
+                {
+
+                    people.Clear();
+                    people.AddRange(dal.ReadDataFromFile());
+                    dataGrid.Items.Refresh();
+
+                }
+                
+            }
+            else
+            {
+
+                MessageBox.Show("Please select a person to modify.");
+
+            }
+        }*/
+
+        private void DeleteData_Click(object sender, RoutedEventArgs e)
+        {
+
+            Person selectedPerson = dataGrid.SelectedItem as Person;
+            if(selectedPerson != null)
+            {
+
+                dal.DeletePerson(selectedPerson.Name);
+
+                people.Clear();
+                people.AddRange(dal.ReadDataFromFile());
+                dataGrid.Items.Refresh();
+
+                MessageBox.Show("Data Deleted Succesfully.");
+
+            }
+            else
+            {
+                MessageBox.Show("Please select a person to delete.");
+            }
+
         }
     }
 }
